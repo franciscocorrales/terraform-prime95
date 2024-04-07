75fa1308e76d66e1dc1403082f650bb31cc00c7a
Francisco Corrales <fracorra@publicisgroupe.net>
2024-04-07 16:33:08 -0600

    add variable descriptions

--- variables.tf
+++ variables.tf
@@ -1,5 +1,6 @@
 variable "contact_emails" {
   type = list(string)
+  description = "Personal email to get alerts"
   default = [
     "your_email@example.com"
   ]
@@ -7,16 +8,18 @@ variable "contact_emails" {
 
 variable "admin_username" {
   type = string
+  description = "Administrator username"
   default = "admin"
 }
 
 variable "admin_password" {
   type = string
+  description = "Administrator Password"
   default = "password"
 }
 
-# mersenne.org username
 variable "mersenne_username" {
   type = string
+  description = "Mersenne.org username"
   default = "anonymous"
 }
