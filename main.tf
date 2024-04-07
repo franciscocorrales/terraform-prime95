33f5602ca9b6e909c335eabfdbe9c45d1d167be1
Francisco Corrales <fracorra@publicisgroupe.net>
2024-04-07 16:09:51 -0600

    fix networking

--- main.tf
+++ main.tf
@@ -28,10 +28,24 @@ resource "azurerm_costmanagement_budget" "example_budget" {
   }
 }
 
+resource "azurerm_virtual_network" "example_vnet" {
+  name                = "prime95-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.prime95_rg.location 
+  resource_group_name = azurerm_resource_group.prime95_rg.name
+}
+
+resource "azurerm_subnet" "azurerm_subnet" {
+  name                 = "prime95-subnet"
+  resource_group_name  = azurerm_resource_group.prime95_rg.name
+  virtual_network_name = azurerm_virtual_network.example_vnet.name
+  address_prefixes     = ["10.0.1.0/24"] 
+}
+
  resource "azurerm_network_interface" "nic" {
   name                  = "prime95-nic"
-  location              = azurerm_resource_group.example.location
-  resource_group_name   = azurerm_resource_group.example.name
+  location              = azurerm_resource_group.prime95_rg.location
+  resource_group_name   = azurerm_resource_group.prime95_rg.name
 
   ip_configuration {
     name                          = "prime95-internal"
