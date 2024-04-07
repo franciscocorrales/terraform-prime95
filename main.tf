c1bb8fc94d472aa8391fb63a42e4e891c930f0eb
Francisco Corrales <fracorra@publicisgroupe.net>
2024-04-07 17:40:02 -0600

    add locals variables

--- main.tf
+++ main.tf
@@ -6,41 +6,48 @@ terraform {
     }
   }
 }
+provider "azurerm" {
+  features {}
+}
+
+locals {
+  project_name = "prime95"
+}
 
 resource "azurerm_resource_group" "prime95_rg" {
-  name     = "prime95-project-rg"
+  name     = "${local.project_name}_rg"
   location = var.region
 }
 
 
 resource "azurerm_virtual_network" "example_vnet" {
-  name                = "prime95-vnet"
+  name                = "${local.project_name}-vnet"
   address_space       = ["10.0.0.0/16"]
   location            = azurerm_resource_group.prime95_rg.location 
   resource_group_name = azurerm_resource_group.prime95_rg.name
 }
 
 resource "azurerm_subnet" "azurerm_subnet" {
-  name                 = "prime95-subnet"
+  name                 = "${local.project_name}-subnet"
   resource_group_name  = azurerm_resource_group.prime95_rg.name
   virtual_network_name = azurerm_virtual_network.example_vnet.name
   address_prefixes     = ["10.0.1.0/24"] 
 }
 
  resource "azurerm_network_interface" "nic" {
-  name                  = "prime95-nic"
+  name                  = "${local.project_name}-nic"
   location              = azurerm_resource_group.prime95_rg.location
   resource_group_name   = azurerm_resource_group.prime95_rg.name
 
   ip_configuration {
-    name                          = "prime95-internal"
+    name                          = "${local.project_name}-internal"
     subnet_id                     = azurerm_subnet.azurerm_subnet.id 
     private_ip_address_allocation = "Dynamic" 
   }
 }
 
 resource "azurerm_linux_virtual_machine" "prime95_vm" {
-  name                  = "prime95-vm"
+  name                  = "${local.project_name}-vm"
   resource_group_name   = azurerm_resource_group.prime95_rg.name
   location              = azurerm_resource_group.prime95_rg.location
   network_interface_ids = [azurerm_network_interface.nic.id]
